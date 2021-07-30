531196bc043e6c57f30d7d8b1c0c290bde599f47
19522515 <19522515@gm.uit.edu.vn>
2021-07-30 10:36:23 +0700

    Initial Setup

---
+++ server.js
@@ -0,0 +1,26 @@
+﻿const express=require('express')
+const customerRouter = require('./routes/customer')
+const indexRouter = require('./routes/index')
+const methodOverride = require('method-override')
+const mongoose =require('mongoose')
+const app=express()
+require('dotenv').config()
+app.use(methodOverride('_method'))
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended: false}))
+
+const connectFunctions=async()=>{
+    try{
+        await mongoose.connect('mongodb+srv://duyprovipwe:067548567@vietdaica.rfdt1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        console.log("Ket noi thanh cong")
+    }catch(e){
+        console.log(e)
+        console.log("Ket noi that bai. Xin hay thu lai")
+    }
+}
+connectFunctions()
+app.use(express.urlencoded({ extended: false }))
+app.use('/customer/',customerRouter)
+
+app.use('/',indexRouter)
+app.listen(process.env.PORT||4000)
